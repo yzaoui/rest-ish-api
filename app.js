f7c31b73ef64ada84113d58c7fef47fcdaf2298d
Yann Zaoui <yzaoui7@gmail.com>
2018-07-20 12:36:22 -0400

    Add mongoose, connect to server, connect product routes

--- app.js
+++ app.js
@@ -2,9 +2,12 @@ const express = require("express");
 const app = express();
 const morgan = require("morgan");
 const bodyParser = require("body-parser");
+const mongoose = require("mongoose");
 
 const productRoutes = require("./api/routes/products");
 const orderRoutes = require("./api/routes/orders");
+
+mongoose.connect(process.env.MONGO_ATLAS);
 
 app.use(morgan("dev"));
 app.use(bodyParser.urlencoded({ extended: false }));
