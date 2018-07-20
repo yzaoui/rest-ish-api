cc3b10029fe7751082b962d12d4e7a173c86bf6b
Yann Zaoui <yzaoui7@gmail.com>
2018-07-19 22:14:21 -0400

    Add more order, product routes

--- api/routes/products.js
+++ api/routes/products.js
@@ -13,4 +13,29 @@ router.post("/", (req, res, next) => {
     });
 });
 
+router.get("/:productId", (req, res, next) => {
+    const id = req.params.productId;
+    if (id === "special") {
+        res.status(200).json({
+            message: "You discovered the special ID"
+        });
+    } else {
+        res.status(200).json({
+            message: "You passed an ID"
+        });
+    }
+});
+
+router.patch("/:productId", (req, res, next) => {
+    res.status(200).json({
+        message: "Updated product!"
+    });
+});
+
+router.delete("/:productId", (req, res, next) => {
+    res.status(200).json({
+        message: "Deleted product!"
+    });
+});
+
 module.exports = router;
