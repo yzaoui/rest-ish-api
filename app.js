b884b4cfd3e2571961e494e4e00003f465fde387
Yann Zaoui <yzaoui7@gmail.com>
2018-07-19 22:38:15 -0400

    Add basic error routing

--- app.js
+++ app.js
@@ -10,4 +10,18 @@ app.use(morgan("dev"));
 app.use("/products", productRoutes);
 app.use("/orders", orderRoutes);
 
+app.use((req, res, next) => {
+    const error = new Error("Not found");
+    error.status = 404;
+    next(error);
+});
+
+app.use((error, req, res, next) => {
+    res.status(error.status || 500).json({
+        error: {
+            message: error.message
+        }
+    });
+});
+
 module.exports = app;
