2851b88e642cea58bf2fa711e4557ca333d2585a
Yann Zaoui <yzaoui7@gmail.com>
2018-07-21 00:03:28 -0400

    Implement retrieval and deletion of specific order by ID

--- api/routes/orders.js
+++ api/routes/orders.js
@@ -61,17 +61,46 @@ router.post("/", (req, res, next) => {
 });
 
 router.get("/:orderId", (req, res, next) => {
-    res.status(200).json({
-        message: "Order details",
-        orderId: req.params.orderId
-    });
+    Order.findById(req.params.orderId)
+        .then(order => {
+            if (!order) return res.status(404).json({ message: "Order not found" });
+
+            res.status(200).json({
+                order: order,
+                request: {
+                    type: "GET",
+                    url: "http://localhost:3000/orders"
+                }
+            })
+        })
+        .catch(err => {
+            res.status(500).json({
+                error: err
+            })
+        });
 });
 
 router.delete("/:orderId", (req, res, next) => {
-    res.status(200).json({
-        message: "Order deleted",
-        orderId: req.params.orderId
-    });
+    Order.remove({ _id: req.params.orderId })
+        .exec()
+        .then(result => {
+            res.status(200).json({
+                message: "Order deleted",
+                request: {
+                    type: "POST",
+                    url: "http://localhost:3000/orders",
+                    body: {
+                        productId: "ObjectId",
+                        quantity: "Number"
+                    }
+                }
+            })
+        })
+        .catch(err => {
+            res.status(500).json({
+                error: err
+            })
+        })
 });
 
 module.exports = router;
