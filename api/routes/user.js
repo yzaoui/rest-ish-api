77fde08e2f6a4bf5a4019500c42d2af942a60b12
Yann Zaoui <yzaoui7@gmail.com>
2018-07-23 14:28:07 -0400

    Install JWT, return token on user login

--- api/routes/user.js
+++ api/routes/user.js
@@ -2,6 +2,7 @@ const express = require("express");
 const router = express.Router();
 const mongoose = require("mongoose");
 const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
 
 const User = require("../models/user");
 
@@ -29,6 +30,40 @@ router.post("/signup", (req, res, next) => {
                         console.error(err);
                         res.status(500).json({ error: err });
                     });
+            });
+        })
+        .catch(err => {
+            console.error(err);
+            res.status(500).json({ error: err });
+        });
+});
+
+router.post("/login", (req, res, next) => {
+    User.find({ username: req.body.username })
+        .exec()
+        .then(users => {
+            if (users.length === 0) return res.status(401).json({ message: "Auth failed" });
+
+            bcrypt.compare(req.body.password, users[0].password, (err, result) => {
+                if (!err && result) {
+                    const token = jwt.sign(
+                        {
+                            username: users[0].username,
+                            userId: users[0]._id
+                        },
+                        process.env.JWT_KEY,
+                        {
+                            expiresIn: "1h"
+                        },
+
+                    );
+                    return res.status(200).json({
+                        message: "Auth successful",
+                        token: token
+                    });
+                }
+
+                return res.status(401).json({ message: "Auth failed" });
             });
         })
         .catch(err => {
